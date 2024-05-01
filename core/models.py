367b20616fa87874c47da8038bb0976f5a5ed938
Amin Amerian <aam.amin.am@gmail.com>
2024-05-01 18:08:49 +0330

    feat(core.models): add Advertisement and Comment models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from utils.models import BaseModel
+
+
+class Advertisement(BaseModel):
+    title = models.CharField(max_length=255, verbose_name=_("Title"))
+    description = models.TextField(verbose_name=_("Description"), blank=True, null=True)
+    created_by = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="advertisements",
+        verbose_name=_("Created by"),
+    )
+
+    class Meta:
+        verbose_name = _("Advertisement")
+        verbose_name_plural = _("Advertisements")
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(BaseModel):
+    advertisement = models.ForeignKey(
+        Advertisement,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name=_("Advertisement"),
+    )
+    text = models.TextField(verbose_name=_("Text"))
+    created_by = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name=_("Created by"),
+    )
+
+    class Meta:
+        verbose_name = _("Comment")
+        verbose_name_plural = _("Comments")
+        unique_together = ("advertisement", "created_by")
+
+    def __str__(self):
+        return self.text
